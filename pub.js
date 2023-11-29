5cf3778491e10ae7790272e98c01ee6e4771ace4
parvez13913 <parvezz13913@gmail.com>
2023-11-29 11:47:05 +0600

    Redis connected

---
+++ pub.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+
+let publisher = redis.createClient({
+    url: 'redis://localhost:6379'
+});
+
+publisher.on('error', (error) => console.log('Redis Error'));
+publisher.on('connect', (error) => console.log('Redis connected'));
+
+const connect = async () => {
+    await publisher.connect();
+}
+
+connect();
+
+app.get('/', (req, res) => {
+    res.send({
+        message: "Publisher active from prot 3001"
+    });
+});
+
+app.get('/publish', async (req, res) => {
+    const id = Math.floor(Math.random() * 10);
+    const data = {
+        id,
+        message: `Message-${id}`
+    };
+
+    await publisher.publish('Message', JSON.stringify(data));
+    res.send({
+        message: "Data published!"
+    });
+});
+
+app.listen(3001, () => {
+    console.log("Publisher server start on 3001");
+});
